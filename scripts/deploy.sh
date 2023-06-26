40208d5915e5888abafe2213bb062dd67383fd96
eujjong18 <76527090+eujjong18@users.noreply.github.com>
2023-06-27 02:52:55 +0900

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -30,4 +30,7 @@ chmod +x $JAR_NAME
 
 echo "> $JAR_NAME 실행"
 
-nohup java -jar -Duser.timezone=Asia/Seoul -Dspring.profiles.active=prod $JAR_NAME >> $REPOSITORY/nohup.out 2>&1 &
+nohup java -jar \
+    -Dspring.profiles.active=prod
+    -Duser.timezone=Asia/Seoul \
+    $JAR_NAME >> $REPOSITORY/nohup.out 2>&1 &
