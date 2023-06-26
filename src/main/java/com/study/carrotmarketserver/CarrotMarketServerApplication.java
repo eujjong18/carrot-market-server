d1bb215f6eed8239b8e6539f4ad132373b0736b8
eujjong18 <eujjong18@naver.com>
2023-06-27 00:05:32 +0900

    first commit

---
+++ src/main/java/com/study/carrotmarketserver/CarrotMarketServerApplication.java
@@ -0,0 +1,13 @@
+package com.study.carrotmarketserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarrotMarketServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarrotMarketServerApplication.class, args);
+	}
+
+}
